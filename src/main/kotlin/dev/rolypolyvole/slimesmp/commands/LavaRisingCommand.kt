879e980b58ee9f264892026aeb047ac36626a418
rolyPolyVole <yezhanting@gmail.com>
2026-02-02 12:24:04 +0000

    Add command to transfer to lava rising server

---
+++ src/main/kotlin/dev/rolypolyvole/slimesmp/commands/LavaRisingCommand.kt
@@ -0,0 +1,40 @@
+package dev.rolypolyvole.slimesmp.commands
+
+import com.mojang.brigadier.context.CommandContext
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.Commands
+import net.minecraft.network.chat.Component
+import net.minecraft.network.protocol.common.ClientboundTransferPacket
+
+class LavaRisingCommand {
+    private val name = "lavarising"
+
+    private val host = "192.168.1.50"
+    private val port = 25565
+
+    fun register() {
+        CommandRegistrationCallback.EVENT.register { dispatcher, registryAccess, environment ->
+            if (!environment.includeDedicated) return@register
+
+            val command = Commands
+                .literal(name)
+                .executes(this::onExecution)
+
+            dispatcher.register(command)
+        }
+    }
+
+    private fun onExecution(context: CommandContext<CommandSourceStack>): Int {
+        val source = context.source
+        val player = source.playerOrException
+
+        source.sendSuccess({ Component.literal("Sending you to Lava Rising!").withColor(0x55FF55) }, false)
+
+        player.connection.send(
+            ClientboundTransferPacket(host, port)
+        )
+
+        return 1
+    }
+}
