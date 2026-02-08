accd0eed30d5fba80ff27c31d8531ce2571e7fbd
rolyPolyVole <yezhanting@gmail.com>
2026-02-08 17:46:22 +0000

    Remove bullshit feature

--- src/main/kotlin/dev/rolypolyvole/slimesmp/SlimeSMPMod.kt
+++ src/main/kotlin/dev/rolypolyvole/slimesmp/SlimeSMPMod.kt
@@ -2,16 +2,6 @@ package dev.rolypolyvole.slimesmp
 
 import dev.rolypolyvole.slimesmp.commands.LavaRisingCommand
 import net.fabricmc.api.DedicatedServerModInitializer
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents
-import net.minecraft.core.registries.Registries
-import net.minecraft.world.InteractionHand
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.EquipmentSlot
-import net.minecraft.world.entity.monster.skeleton.Skeleton
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
-import net.minecraft.world.item.enchantment.Enchantments
-import net.minecraft.world.level.Level
 
 
 class SlimeSMPMod : DedicatedServerModInitializer {
@@ -20,24 +10,5 @@ class SlimeSMPMod : DedicatedServerModInitializer {
         println("Slime SMP Mod initialized!")
 
         LavaRisingCommand().register()
-
-        ServerEntityEvents.ENTITY_LOAD.register(this::onEntityLoad)
-    }
-
-    private fun onEntityLoad(entity: Entity, world: Level) {
-        if (entity !is Skeleton) return
-        if (Math.random() > 0.02) return
-
-        val sword = ItemStack(Items.IRON_SWORD).apply {
-            val registry = world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT)
-            val sharpnessHolder = registry.getOrThrow(Enchantments.SHARPNESS)
-
-            enchant(sharpnessHolder, 1)
-        }
-
-        val helmet = ItemStack(Items.IRON_HELMET)
-
-        entity.setItemInHand(InteractionHand.MAIN_HAND, sword)
-        entity.setItemSlot(EquipmentSlot.HEAD, helmet)
     }
 }
