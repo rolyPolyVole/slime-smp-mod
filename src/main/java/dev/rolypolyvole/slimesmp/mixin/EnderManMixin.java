9ceea1be9c3ba0555665ee872a827866dbf23425
rolyPolyVole <yezhanting@gmail.com>
2026-03-05 21:58:22 +0000

    Prevent endermen from targeting endermites during dragon fight

--- src/main/java/dev/rolypolyvole/slimesmp/mixin/EnderManMixin.java
+++ src/main/java/dev/rolypolyvole/slimesmp/mixin/EnderManMixin.java
@@ -1,8 +1,13 @@
 package dev.rolypolyvole.slimesmp.mixin;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.monster.Endermite;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,10 +15,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EnderMan.class)
 @SuppressWarnings("unused")
-abstract class EnderManMixin {
+abstract class EnderManMixin extends Monster {
+
+    protected EnderManMixin(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @Inject(method = "setTarget", at = @At("HEAD"), cancellable = true)
     private void preventDragonTarget(LivingEntity target, CallbackInfo ci) {
         if (target instanceof EnderDragon) {
+            ci.cancel();
+        }
+
+        if (!((ServerLevel) level()).getDragons().isEmpty() && target instanceof Endermite) {
             ci.cancel();
         }
     }
