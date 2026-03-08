274426e66480ca900e0113fb6ee7efe265232039
rolyPolyVole <yezhanting@gmail.com>
2026-03-08 18:20:26 +0000

    Replace vanilla skeletons with crystal protectors & format code

--- src/main/java/dev/rolypolyvole/slimesmp/mixin/EndCrystalMixin.java
+++ src/main/java/dev/rolypolyvole/slimesmp/mixin/EndCrystalMixin.java
@@ -9,13 +9,16 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.boss.enderdragon.EndCrystal;
+import net.minecraft.world.entity.monster.skeleton.Skeleton;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -23,14 +26,36 @@ import java.util.List;
 @Mixin(EndCrystal.class)
 public abstract class EndCrystalMixin {
 
+    @Unique
+    private EndCrystal self() {
+        return (EndCrystal)(Object) this;
+    }
+
     @Redirect(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/BaseFireBlock;getState(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
     private BlockState useSoulFire(BlockGetter blockGetter, BlockPos blockPos) {
         return Blocks.SOUL_FIRE.defaultBlockState();
     }
 
+    @Inject(method = "tick()V", at = @At("TAIL"))
+    private void reloadCrystalProtectors(CallbackInfo ci) {
+        EndCrystal self = self();
+        if (!(self.level() instanceof ServerLevel serverLevel)) return;
+
+        for (Entity passenger : self.getPassengers()) {
+            if (passenger instanceof Skeleton && !(passenger instanceof CrystalProtector)) {
+                passenger.discard();
+
+                CrystalProtector protector = new CrystalProtector(serverLevel);
+                protector.equipArmor();
+                protector.startRiding(self, true, false);
+                serverLevel.addFreshEntity(protector);
+            }
+        }
+    }
+
     @Inject(method = "hurtServer", at = @At("HEAD"), cancellable = true)
     private void preventDamageIfGuarded(ServerLevel serverLevel, DamageSource damageSource, float amount, CallbackInfoReturnable<Boolean> cir) {
-        EndCrystal self = (EndCrystal)(Object) this;
+        EndCrystal self = self();
 
         if (self instanceof CrystalProtector.OrbitCrystal) return;
 
